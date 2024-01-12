7709f1dacb1b84cfb47283ad6c5951bfc53f1b1e
ildarghc <ildarpochta@mail.ru>
2024-01-12 22:21:20 +0500

    "Функции в Python.Функция с параметром"

---
+++ 2_func.py
@@ -0,0 +1,9 @@
+def print_params(param):
+    for _ in range(2):
+        print(param)
+
+
+for param in ('param', 'param-pam-pam'):
+    print_params(param)
+for param in ('purum', 'purum-pum-pum'):
+    print_params(param)
